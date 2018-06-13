50521e30a90f21cbe972c0d4170f0eabf4edd44a
anto-christo <anto.christo.20@gmail.com>
2018-06-13 13:29:52 +0530

    Add homepage

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { getJSConfig, getCSSConfig } = require('frappejs/config/rollup');
-
-module.exports = [
-    getJSConfig({
-        input: path.resolve(__dirname, 'src/index.js'),
-        output: path.resolve(__dirname, 'dist/bundle.js'),
-    })
-]

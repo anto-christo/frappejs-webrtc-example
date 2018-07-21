2befef7b15634211dc4e58443ab398279ba3fe06
anto-christo <anto.christo.20@gmail.com>
2018-07-21 22:38:26 +0530

    Fix port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = require('frappejs/server');
 server.start({
     backend: 'sqlite',
     connectionParams: {
-	  port: 9000,
+	  port: 8000,
       dbPath: 'test.db',
     },
     staticPath: './dist',

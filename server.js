397baa2dcf4551f023b0d579fe7c3395da636c0a
anto-christo <anto.christo.20@gmail.com>
2018-07-21 22:36:01 +0530

    Revert back to simple  example

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = require('frappejs/server');
 server.start({
     backend: 'sqlite',
     connectionParams: {
+	  port: 9000,
       dbPath: 'test.db',
     },
     staticPath: './dist',
